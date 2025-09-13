f4d36e0436786b9df1ed4dea8b22b686157d1c12
muttyqt15 <hayaye69@gmail.com>
2025-09-13 18:14:04 +0700

    feat: init api using gin

--- apps/api/main.go
+++ apps/api/main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"log"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
+
+	router.GET("/health", func(c *gin.Context) {
+		data := gin.H{"lang": "GO语言"}
+		c.AsciiJSON(http.StatusOK, gin.H{"data": data})
+	})
+
+	err := router.Run(":8080")
+	if err != nil {
+		log.Fatal("error on starting api")
+	}
 }
